3bab9572602c697c991ef1a48cd7d4d8295be1c0
MichaelYeh507 <54426940+MichaelYeh507@users.noreply.github.com>
2020-06-28 12:58:16 -0700

    guestbook, integerstack(ip)

---
+++ src/_01_IntroToArrayLists/GuestBookRunner.java
@@ -0,0 +1,10 @@
+package _01_IntroToArrayLists;
+
+public class GuestBookRunner {
+
+		static _02_GuestBook guest = new _02_GuestBook();
+		public static void main(String[] args) {
+			guest.run();
+		}
+	}
+
--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,6 +1,14 @@
 package _01_IntroToArrayLists;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class _02_GuestBook {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,7 +17,33 @@ public class _02_GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
-	
-	
-	
+	ArrayList<String> names = new ArrayList<String>();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton addName = new JButton();
+	JButton viewName = new JButton();
+	public void run() {
+		frame.add(panel);
+		frame.setVisible(true);
+		panel.add(addName);
+		panel.add(viewName);
+		frame.pack();
+		addName.setText("Add a Guest.");
+		viewName.setText("View current guests.");
+		addName.addActionListener(this);
+		viewName.addActionListener(this);
+	}
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource()==addName) {
+			String newUser = JOptionPane.showInputDialog("Enter the guest name.");
+			names.add(newUser);
+		} else {
+			for (int i = 0; i < names.size(); i++) {
+				String s = names.get(i);
+				System.out.println(s);
+			}
+		}
+	}
 }
+	
