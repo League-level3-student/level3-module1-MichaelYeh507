c895223ea4af4b390f089f021d9b59fde8cd012f
MichaelYeh507 <54426940+MichaelYeh507@users.noreply.github.com>
2020-07-26 11:31:57 -0700

    Hangman

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -38,12 +38,12 @@ public class HangMan implements KeyListener{
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
 	@Override
 	public void keyReleased(KeyEvent e) {
